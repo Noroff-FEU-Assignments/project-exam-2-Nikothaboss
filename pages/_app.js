2625eae559afe6d093eeb004e59229b199a47f63
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-04-07 18:18:33 +0200

    feat: added a new graphQL client with the refreshToken url. Not sure if this is the correct way

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ClientWrapper } from "../contexts/clientContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClientWrapper>
+      <Component {...pageProps} />
+    </ClientWrapper>
+  );
 }
 
-export default MyApp
+export default MyApp;
