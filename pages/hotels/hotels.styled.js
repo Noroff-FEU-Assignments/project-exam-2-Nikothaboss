7d3296fe7ede251dddc511f764d3ee55bd5f456b
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-05-06 14:11:00 +0200

    feat: started on hotels page

---
+++ pages/hotels/hotels.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { device } from "../../styles/styleVariables";
+export const HotelsStyled = styled.main`
+  padding: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .card {
+    width: 24%;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    margin-bottom: 1rem;
+
+    @media${device.laptop} {
+      width: 49%;
+      margin-bottom: 1rem;
+    }
+    @media${device.mobileL} {
+      width: 100%;
+    }
+  }
+`;
---
+++ pages/hotels/index.jsx
@@ -0,0 +1,39 @@
+import { baseUrl } from "../../utils/API_CONSTANTS";
+import { HotelsStyled } from "./hotels.styled";
+import Image from "next/image";
+const Hotels = ({ hotels }) => {
+  return (
+    <HotelsStyled>
+      {hotels.map((d) => {
+        const data = d.attributes;
+        return (
+          <div className="card">
+            <Image
+              src={data.main_img}
+              layout="responsive"
+              width="300"
+              height="200"
+            />
+            <h2>{data.name}</h2>
+          </div>
+        );
+      })}
+    </HotelsStyled>
+  );
+};
+
+export default Hotels;
+
+export const getStaticProps = async () => {
+  const hotelsResponse = await fetch(baseUrl + "hotels");
+
+  const hotels = await hotelsResponse.json();
+
+  console.log(hotels.data);
+
+  return {
+    props: {
+      hotels: hotels.data,
+    },
+  };
+};
