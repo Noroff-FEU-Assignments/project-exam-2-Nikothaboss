2be0bd89b0b3d0fd3a59205223cd5c1532a41c17
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-05-19 10:43:06 +0200

    feat: started on details page for activities

--- pages/activities/[id].jsx
+++ pages/activities/[id].jsx
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { baseUrl } from "../../utils/API_CONSTANTS";
 
-const Hotel = ({ data }) => {
+const Acitvity = ({ data }) => {
   console.log(data);
   return <div>{data.name}</div>;
 };
 
-export default Hotel;
+export default Acitvity;
 
 export const getServerSideProps = async ({ query }) => {
   const { id } = query;
-  const res = await axios.get(baseUrl + "hotels/" + id);
+  const res = await axios.get(baseUrl + "activities/" + id);
   const data = res.data.data.attributes;
 
   return {
--- pages/activities/index.jsx
+++ pages/activities/index.jsx
@@ -1,21 +1,24 @@
 import { baseUrl } from "../../utils/API_CONSTANTS";
 import { HotelsStyled } from "../hotels/hotels.styled";
 import Image from "next/image";
+import Link from "next/link";
 const Hotels = ({ hotels }) => {
   return (
     <HotelsStyled>
       {hotels.map((d) => {
         const data = d.attributes;
         return (
-          <div className="card" key={d.id}>
-            <Image
-              src={data.main_img}
-              layout="responsive"
-              width="300"
-              height="200"
-            />
-            <h2>{data.name}</h2>
-          </div>
+          <Link href={`/activities/${d.id}`}>
+            <div className="card" key={d.id}>
+              <Image
+                src={data.main_img}
+                layout="responsive"
+                width="300"
+                height="200"
+              />
+              <h2>{data.name}</h2>
+            </div>
+          </Link>
         );
       })}
     </HotelsStyled>
