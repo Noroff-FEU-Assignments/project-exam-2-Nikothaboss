9ab671a49c1ecd71dd5645bff4ca56ecf65af900
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-05-23 12:22:00 +0200

    feat: added dropdown on searchbar when typing

--- components/searchBar/SearchBar.jsx
+++ components/searchBar/SearchBar.jsx
@@ -1,5 +1,7 @@
 import { SearchBarStyled } from "./searchBar.styled";
 import { useState, useRef, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const SearchBar = ({ data }) => {
   const searchRef = useRef();
@@ -21,7 +23,32 @@ const SearchBar = ({ data }) => {
         ref={searchRef}
         value={searchWord}
         onChange={() => setSearchWord(searchRef.current.value)}
+        onBlur={() => {
+          setTimeout(() => {
+            setSearchWord("");
+          }, 100);
+        }}
       />
+      <div className="filterResults">
+        {searchWord.length !== 0 &&
+          filterData.map((d) => {
+            const data = d.attributes;
+            return (
+              <Link key={d.id} href={`/hotels/${d.id}`}>
+                <div>
+                  <Image
+                    src={data.main_img}
+                    layout="responsive"
+                    height="100"
+                    width="200"
+                    className="searchImg"
+                  />
+                  <h3>{data.name}</h3>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
     </SearchBarStyled>
   );
 };
--- components/searchBar/searchBar.styled.js
+++ components/searchBar/searchBar.styled.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-import { device } from "../../styles/styleVariables";
+import { colors, device } from "../../styles/styleVariables";
 
 export const SearchBarStyled = styled.div`
   width: 33%;
+  position: relative;
 
   @media${device.tablet} {
     width: 75%;
@@ -10,5 +11,29 @@ export const SearchBarStyled = styled.div`
   input {
     padding: 0.5rem;
     width: 100%;
+  }
+
+  div {
+    left: 0;
+    /* width: 100%; */
+    /* height: 100%; */
+  }
+
+  .searchImg {
+    object-fit: cover;
+  }
+
+  .filterResults {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    max-height: 500px;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    width: 100%;
+    overflow-y: scroll;
+    background: ${colors.primary};
+    z-index: 999;
   }
 `;
